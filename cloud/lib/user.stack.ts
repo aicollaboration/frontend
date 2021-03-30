91ec04df7185cb165b995d1c8ea82847961f22dd
Tobias Oberrauch <tobiasoberrauch@gmx.de>
2021-03-30 09:11:49 +0200

    remove obsolete

--- cloud/lib/user.stack.ts
+++
@@ -1,8 +0,0 @@
-import { Construct, Stack, StackProps } from '@aws-cdk/core';
-
-export class UserStack extends Stack {
-    constructor(scope: Construct, id: string, props?: StackProps) {
-        super(scope, id, props);
-
-    }
-}
