5ca5397e90d4fe7937eec73197970e8adaac5f09
Tobias Oberrauch <tobias.oberrauch@gmx.de>
2021-07-01 17:39:12 +0200

    add service-endpoint

--- src/app/modules/admin/components/admin-overview/admin-overview.component.ts
+++ src/app/modules/admin/components/admin-overview/admin-overview.component.ts
@@ -1,4 +1,7 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { ICreateOrderRequest, IPayPalConfig } from 'ngx-paypal';
+
+declare const paypal;
 
 @Component({
     selector: 'admin-overview',
@@ -7,5 +10,74 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
     encapsulation: ViewEncapsulation.None,
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AdminOverviewComponent {
+export class AdminOverviewComponent implements OnInit {
+    public payPalConfig?: IPayPalConfig;
+
+    @ViewChild('paypal', { static: true })
+    public paypalElement: ElementRef;
+
+    public ngOnInit(): void {
+        paypal.Buttons().render();
+
+        this.initConfig();
+    }
+
+    private initConfig(): void {
+        this.payPalConfig = {
+            currency: 'EUR',
+            clientId: 'AW2knlF9Yu_sx7-pEUznxA3246h-RvDdbLnIHi8RxJG_QA91blJ56KDSRK81c9RhDsbHaebmLWULRUtg',
+            createOrderOnClient: (data) => <ICreateOrderRequest>{
+                intent: 'CAPTURE',
+                purchase_units: [{
+                    amount: {
+                        currency_code: 'EUR',
+                        value: '9.99',
+                        breakdown: {
+                            item_total: {
+                                currency_code: 'EUR',
+                                value: '9.99'
+                            }
+                        }
+                    },
+                    items: [{
+                        name: 'Enterprise Subscription',
+                        quantity: '1',
+                        category: 'DIGITAL_GOODS',
+                        unit_amount: {
+                            currency_code: 'EUR',
+                            value: '9.99',
+                        },
+                    }]
+                }]
+            },
+            advanced: {
+                commit: 'true'
+            },
+            style: {
+                label: 'paypal',
+                layout: 'vertical'
+            },
+            onApprove: (data, actions) => {
+                console.log('onApprove - transaction was approved, but not authorized', data, actions);
+                actions.order.get().then(details => {
+                    console.log('onApprove - you can get full order details inside onApprove: ', details);
+                });
+
+            },
+            onClientAuthorization: (data) => {
+                console.log('onClientAuthorization - you should probably inform your server about completed transaction at this point', data);
+
+            },
+            onCancel: (data, actions) => {
+                console.log('OnCancel', data, actions);
+
+            },
+            onError: err => {
+                console.log('OnError', err);
+            },
+            onClick: (data, actions) => {
+                console.log('onClick', data, actions);
+            }
+        };
+    }
 }
